4e0bb05ff10b6c525cd41672a86bfa4f68c0306d
nhwangbo <nickhwangbo@gmail.com>
2020-12-19 01:19:16 -0800

    Start adding TCG code

---
+++ src/tcg/TcgDeck.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+public class TcgDeck : ROMObject {
+
+    public List<TcgCard> Cards;
+
+    public TcgDeck(Tcg game, byte id, ByteStream data) {
+        Id = id;
+        byte count;
+        Cards = new List<TcgCard>();
+
+        do {
+            count = data.u8();
+            if (count > 0) {
+                for (int j = 0; j < count; j++) {
+                    Cards.Add(game.Cards[data.Peek()]);
+                }
+                data.Seek(1);
+            }
+        } while (count != 0);
+
+
+        Name = game.GetTextFromId(data.u16le());
+    }
+}
