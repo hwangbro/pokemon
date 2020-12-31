41abf695b4c45257232bf614c4efa615ed0a84b3
nhwangbo <nickhwangbo@gmail.com>
2020-12-31 00:21:01 -0800

    main

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,45 @@
-﻿using System.IO;
-using System.Threading;
-using System.Linq;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Numerics;
+﻿using System;
+
 
 class Program {
     static void Main(string[] args) {
-        Totodile.StartSearch(1);
+    }
+
+    public static void RedTest() {
+        Red gb = new Red();
+        gb.Record("test");
+
+        RbyIntroSequence sequence = new RbyIntroSequence();
+        sequence.Add(gb.IntroStrats["nopal"]);
+        sequence.Add(gb.IntroStrats["gfSkip"]);
+        sequence.Add(gb.IntroStrats["hop0"]);
+        sequence.Add(gb.IntroStrats["title0"]);
+        sequence.Add(gb.IntroStrats["cont"]);
+        sequence.Add(gb.IntroStrats["cont"]);
+
+        gb.ExecuteIntroSequence(sequence);
+        gb.Execute("L L L U L L U A U L A L D L D L L D A D D A D D D L A L L A L U U A U"); // regular nido
+        gb.Yoloball();
+        // gb.Execute("L D U A L L U L L L L A U L L L L L A D D A D D L A D D D L A U U A U"); // pal nido
+        Console.WriteLine(sequence);
+        gb.AdvanceFrames(1000);
+        gb.Dispose();
+    }
+
+    public static void YellowTest() {
+        Yellow gb = new Yellow(false, "basesaves/yellow/nido.sav");
+        gb.Record("test");
+        RbyIntroSequence sequence = new RbyIntroSequence();
+        sequence.Add(gb.IntroStrats["gfSkip"]);
+        sequence.Add(gb.IntroStrats["intro1"]);
+        sequence.Add(gb.IntroStrats["title0"]);
+        sequence.Add(gb.IntroStrats["cont"]);
+        sequence.Add(gb.IntroStrats["cont"]);
+
+        gb.ExecuteIntroSequence(sequence);
+        gb.Execute("U R A R U");
+        gb.Yoloball();
+        gb.AdvanceFrames(1000);
+        gb.Dispose();
     }
 }
