10b171e5acb262a5a73f40b6afdead99726161e3
nhwangbo <nickhwangbo@gmail.com>
2020-12-31 00:18:08 -0800

    Add ability to pass basesave to gb init

--- src/games/gsc/Crystal.cs
+++ src/games/gsc/Crystal.cs
@@ -1,4 +1,4 @@
 public class Crystal : Gsc {
 
-    public Crystal(bool speedup = false) : base("roms/pokecrystal.gbc", speedup ? SpeedupFlags.NoVideo | SpeedupFlags.NoSound : SpeedupFlags.None) { }
+    public Crystal(bool speedup = false, string saveName = "roms/pokecrystal.sav") : base("roms/pokecrystal.gbc", saveName, speedup ? SpeedupFlags.NoVideo | SpeedupFlags.NoSound : SpeedupFlags.None) { }
 }
