b65212eba22f626d48606d79f39938a170441200
nhwangbo <nickhwangbo@gmail.com>
2021-01-12 02:42:58 -0800

    GSC Item update
    GscBag and item execution pointers

--- src/games/gsc/GscItem.cs
+++ src/games/gsc/GscItem.cs
@@ -1,11 +1,15 @@
 public class GscItem : ROMObject {
 
     public Gsc Game;
+    public int ExecutionPointer;
+    public string ExecutionPointerLabel;
 
     public GscItem(Gsc game, byte id, ByteStream name) {
         Game = game;
         Name = game.Charmap.Decode(name.Until(Charmap.Terminator));
         Id = id;
+        ExecutionPointer = 0x3 << 16 | game.ROM.u16le(game.SYM["ItemEffects"] + (byte) id * 2);
+        if(game.SYM.Contains(ExecutionPointer)) ExecutionPointerLabel = game.SYM[ExecutionPointer];
     }
 }
 
